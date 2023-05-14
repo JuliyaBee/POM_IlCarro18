d36ef049b26eda6af3a8cd9f62bf4d9bdaa34c52
JuliyaBee <juliyabee@gmail.com>
2023-05-14 19:01:31 +0300

    QA-18

---
+++ src/main/java/screens/BaseScreen.java
@@ -0,0 +1,21 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BaseScreen
+{
+    AppiumDriver<MobileElement> driver;
+
+    public BaseScreen(AppiumDriver<MobileElement> driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+
+    }
+}
